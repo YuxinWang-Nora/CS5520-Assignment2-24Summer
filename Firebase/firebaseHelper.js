8a49cdfbb4e1689a8baf1200c3a54c4b580b336e
Yuxin(Nora) Wang <156369896+YuxinWang-Nora@users.noreply.github.com>
2024-07-26 01:13:36 -0700

    Added an deletFromDB method to delet an document from an collection

--- Firebase/firebaseHelper.js
+++ Firebase/firebaseHelper.js
@@ -1,9 +1,19 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc } from "firebase/firestore";
 import { database } from "./firebaseSetup";
 
 export async function writeToDB(data, collectionName) {
     try {
         await addDoc(collection(database, collectionName), data);
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+export async function deleteFromDB(id, collectionName) {
+    try {
+        await deleteDoc(doc(database, collectionName, id));
+        console.log(collectionName, id, "successfully deleted!");
     }
     catch (err) {
         console.log(err)
